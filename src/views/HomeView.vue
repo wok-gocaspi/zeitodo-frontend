0616fe49b9edd3e01d881124db63a759df6a4c50
Luca Wolckenhauer <l.wolckenhauer@gocaspi.de>
2022-08-22 14:56:38 +0200

    added router and vuetify

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template> 
+  <div>
+  <hello-world />
+  </div>
+</template>
+
+<script>
+  import HelloWorld from '../components/HelloWorld'
+
+  export default {
+    name: 'HomeView',
+
+    components: {
+      HelloWorld,
+    },
+  }
+</script>
