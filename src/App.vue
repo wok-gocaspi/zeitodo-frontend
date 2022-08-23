8566c6ad27fa15e1e01ca5409cbda1e8cd99cdcd
nil <n.spiller@gocaspi.de>
2022-08-23 16:59:56 +0200

    feature/ON4T-96_FrontendForApp_generalOutline

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <v-app>
     <v-navigation-drawer app v-model="drawer">
       <v-list dense nav>
-        <v-list-item v-for="item in items" v-bind:key="item.title" link>
+        <v-list-item v-for="item in items" v-bind:key="item.title" link :to="item.path">
           <v-list-item-icon>
             <v-icon>{{item.icon}}</v-icon>
           </v-list-item-icon>
@@ -16,27 +16,89 @@
     </v-navigation-drawer>
     <v-app-bar app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+<div class="toolbar-btn">
+        <div class="text-center">
+          <v-menu offset-y>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                  elevation="2"
+                  fab
+                  icon
+                  color="primary"
+                  dark
+                  v-bind="attrs"
+                  v-on="on"
+              >
+                Profil
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item
+                  v-for="(opt, index) in profilBtn"
+                  :key="index"
+                  v-on:click="loginDialog = true"
+              >
+                <v-list-item-icon>
+                  <v-icon>{{opt.icon}}</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                  <v-list-item-title>{{opt.title}}</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </div>
+</div>
     </v-app-bar>
     <v-main>
+      <input v-model="username"/>
+      <button v-on:click="getLoggedinUser()">get user</button>
+      <LoginDialog @closed="loginDialog=false" v-if="loginDialog"></LoginDialog>
       <v-container fluid>
+
         <router-view></router-view>
+
       </v-container>
     </v-main>
+
+
   </v-app>
 </template>
 
 <script>
+import userservice from "@/services/userService";
+import LoginDialog from "@/components/LoginDialog";
+
+
+
 export default {
   name: 'App',
+  components: {LoginDialog},
 
   data: () => ({
     items: [
-      { title: 'Dashboard', icon: 'mdi-view-dashboard' },
+      { title: 'Dashboard', icon: 'mdi-view-dashboard', path:"/" },
     ],
-    drawer: false
+    profilBtn:[
+      {title: "Einstellungen", icon:"mdi-view-dashboard"},
+      {title: "Login", icon:"mdi-view-dashboard"}
+    ],
+    drawer: false,
+
+  username: '',
+    loginDialog: false,
+
   }),
   methods: {
-
+    async getLoggedinUser() {
+      this.success = await userservice.getLoggedinUser(this.username,this.username);
+    },
   }
 };
 </script>
+<style scoped>
+.toolbar-btn{
+ position: absolute;
+  right: 2rem;
+}
+</style>
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,5 +1,20 @@
-function test(){
-    console.log("hello world")
-}
+import axios from 'axios'
 
-module.exports = {test}
+
+
+
+
+
+
+export default{
+
+    getLoggedinUser(username, password){
+        return new Promise((res,rej)=>{
+            let UserPayload = {username: username, password: password}
+            axios.post("http://localhost:9090/login",JSON.stringify(UserPayload))
+                .then(response => res(response.data) )
+                .catch(error => rej(error))
+        })
+
+    }
+}
