0a755f039a34fc44f306751b0ab76493306a1039
Luca Wolckenhauer <l.wolckenhauer@gocaspi.de>
2022-08-28 17:42:23 +0200

    changed userserice tests

--- tests/unit/userService.spec.js
+++ tests/unit/userService.spec.js
@@ -1,22 +1,38 @@
 // import { shallowMount } from "@vue/test-utils";
 // import flushPromises from "flush-promises";
 import userService from "@/services/userService";
+import axios from "axios"
 
-jest.mock("axios", () => ({
-    post: () => Promise.resolve({ token: "fakeToken" })
-}));
+jest.mock("axios")
 
-describe("Posts.vue", () => {
-    it("mocking the axios call to post correct username and password", async () => {
-  //      var wrapper = shallowMount(userService);
-       let res =   userService.getLoggedinUser("Peter","123")
-        await res
-        let data
-        res.then(response=> data = response)
-        await data
+describe("loginUser", () => {
+    describe("login was successful", () => {
+        it("send user data", async () => {
+            const loginForm = {
+                username: "Peter",
+                password: "123"
+            }
+            const token = "123456789"
+            axios.post.mockResolvedValueOnce(token)
 
- //       await flushPromises();
-   //     expect(data).toBe({ token: "fakeToken" });
-       expect(res).toBeTruthy()
-    });
-});
+            const result = await userService.getLoggedinUser(loginForm.username, loginForm.password)
+
+            expect(result).toEqual(token)
+        })
+    })
+    describe("login failed", () => {
+        it("send invalid user data", async () => {
+            const loginForm = {
+                username: "Peter",
+                password: "123"
+            }
+            var someerror = new Error("invalid data")
+            axios.post.mockRejectedValueOnce(someerror)
+
+            const result = await userService.getLoggedinUser(loginForm.username, loginForm.password)
+
+            expect(result).toEqual(someerror)
+        })
+    })
+})
+
