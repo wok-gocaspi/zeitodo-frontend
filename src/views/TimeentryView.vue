5ec716ea55687162cb245813686b93ea9c48a2c1
miled <m.souissi@gocaspi.de>
2022-09-19 08:50:49 +0200

    Frontend for Time entry

--- src/views/TimeentryView.vue
+++ src/views/TimeentryView.vue
@@ -9,19 +9,28 @@
   <h1>Stundentafel </h1>
     </v-card-title>
 </v-container>
-
-
+    <v-row
+      align="center"
+      >
   <v-card-text>
     <v-btn
         outlined
         class="mr-2"
-        color="grey darken-2"
+        color="primary"
         @click="setToday"
-    >
-      <h4><p align="center"> &#43;Zeiteintrag </p></h4>
-    </v-btn>
-  </v-card-text>
+        v-model="right"
+        label="Right"
 
+    >
+       &#43;Zeiteintrag
+      <v-icon
+        dark
+        right
+        ></v-icon>
+    </v-btn>
+
+  </v-card-text>
+    </v-row>
   </v-card>
 
   <v-card
@@ -47,7 +56,10 @@
               @click="setToday"
               >
               Heute
+
             </v-btn>
+
+
             <v-btn
               fab
               text
@@ -139,7 +151,94 @@
 </template>
 
 <script>
+
 export default {
   name: "TimeentryView.vue",
+  data: () => ({
+    focus: '',
+    type: 'month',
+    typeToLabel: {
+      month: 'Month',
+      week: 'Week',
+      day: 'Day',
+      '4day': '4 Days',
+    },
+    selectedEvent: {},
+    selectedElement: null,
+    selectedOpen: false,
+    events: [],
+    colors: ['blue', 'indigo', 'deep-purple', 'cyan', 'green', 'orange', 'grey darken-1'],
+    names: ['Meeting', 'Holiday', 'PTO', 'Travel', 'Event', 'Birthday', 'Conference', 'Party'],
+  }),
+  mounted () {
+    this.$refs.calendar.checkChange()
+  },
+  methods: {
+    viewDay ({ date }) {
+      this.focus = date
+      this.type = 'day'
+    },
+    getEventColor (event) {
+      return event.color
+    },
+    setToday () {
+      this.focus = ''
+    },
+    prev () {
+      this.$refs.calendar.prev()
+    },
+    next () {
+      this.$refs.calendar.next()
+    },
+    showEvent ({ nativeEvent, event }) {
+      const open = () => {
+        this.selectedEvent = event
+        this.selectedElement = nativeEvent.target
+        requestAnimationFrame(() => requestAnimationFrame(() => this.selectedOpen = true))
+      }
+
+      if (this.selectedOpen) {
+        this.selectedOpen = false
+        requestAnimationFrame(() => requestAnimationFrame(() => open()))
+      } else {
+        open()
+      }
+
+      nativeEvent.stopPropagation()
+    },
+    updateRange ({ start, end }) {
+      const events = []
+
+      const min = new Date(`${start.date}T00:00:00`)
+      const max = new Date(`${end.date}T23:59:59`)
+      const days = (max.getTime() - min.getTime()) / 86400000
+      const eventCount = this.rnd(days, days + 20)
+
+      for (let i = 0; i < eventCount; i++) {
+        const allDay = this.rnd(0, 3) === 0
+        const firstTimestamp = this.rnd(min.getTime(), max.getTime())
+        const first = new Date(firstTimestamp - (firstTimestamp % 900000))
+        const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000
+        const second = new Date(first.getTime() + secondTimestamp)
+
+        events.push({
+          name: this.names[this.rnd(0, this.names.length - 1)],
+          start: first,
+          end: second,
+          color: this.colors[this.rnd(0, this.colors.length - 1)],
+          timed: !allDay,
+        })
+      }
+
+      this.events = events
+    },
+    rnd (a, b) {
+      return Math.floor((b - a + 1) * Math.random()) + a
+    },
+  },
 }
+
+/*export default {
+  name: "TimeentryView.vue",
+}*/
 </script>
