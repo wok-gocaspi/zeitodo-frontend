e8749c23d3621de866f80cbf522243eaea5c4666
miled <m.souissi@gocaspi.de>
2022-09-22 08:47:26 +0200

    Frontend for Time entry update calendar with test

--- tests/unit/timeentryService.spec.js
+++ tests/unit/timeentryService.spec.js
@@ -4,6 +4,8 @@
 import timeentryService from "@/services/timeentryService";
 
 import axios from "axios"
+import moment from "moment-timezone";
+
 
 
 jest.mock("axios")
@@ -27,3 +29,71 @@ describe("Axios Requests", () => {
         })
 
     })
+describe("Axios Requests",()=>{
+
+    it("get time entry", async () => {
+    const gettime = {
+        startDate: Date.now().toString(),
+        endDate: Date.now().toString(),
+        breakStartDate: Date.now().toString(),
+        breakEndDate: Date.now().toString(),
+    }
+    const userid = "6317793bd4a3cd6e77d99360"
+    axios.get.mockResolvedValueOnce(userid)
+
+    axios.get.mockResolvedValueOnce(JSON.stringify(gettime))
+    const result = await timeentryService.getTimeentry(JSON.stringify(gettime))
+    expect(result).toEqual(JSON.stringify(gettime))
+})
+
+
+})
+
+describe("Axios Requests",()=>{
+
+
+    it("update time", async () => {
+
+        const update = {
+            start: new Date(Date.now().toString()),
+            end: new Date(Date.now().toString()),
+            breakStart: new Date(Date.now().toString()),
+            breakEnd:new Date(Date.now().toString()),
+        }
+        const userid = "6317793bd4a3cd6e77d99360"
+        axios.get.mockResolvedValueOnce(userid)
+
+        axios.put.mockResolvedValueOnce(JSON.stringify(update))
+        const result = await timeentryService.updatetimeentry(JSON.stringify(update))
+        expect(result).toEqual(JSON.stringify(update))
+    })
+
+
+})
+describe("Axios Requests",()=>{
+
+    it("get time", async () => {
+
+        const get = Date.now().toString()
+        let UTCtime=moment(get).tz("Europe/Berlin").format("HH:mm")
+
+
+        const result = await timeentryService.gettime(UTCtime)
+        expect(result).toEqual(UTCtime)
+
+    })
+
+
+})
+describe("Axios Requests",()=>{
+
+    it("get date", async () => {
+        const getdate = Date.now().toString()
+        let UTCdate=moment(getdate).tz("Europe/Berlin").format("YYYY-MM-DD")
+
+        const result = await timeentryService.getdate(UTCdate)
+        expect(result).toEqual(UTCdate)
+    })
+
+
+})
