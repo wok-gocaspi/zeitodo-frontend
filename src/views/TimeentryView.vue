688dad86c223b4ffd35e6dbe3d88de84f33761ad
miled <m.souissi@gocaspi.de>
2022-10-05 15:06:47 +0200

    Frontend for Stunden-konto with update

--- src/views/TimeentryView.vue
+++ src/views/TimeentryView.vue
@@ -259,9 +259,7 @@
               </v-btn>
               <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
               <v-spacer></v-spacer>
-              <v-btn icon>
-                <v-icon>mdi-heart</v-icon>
-              </v-btn>
+
               <v-btn icon>
                 <v-icon>mdi-dots-vertical</v-icon>
               </v-btn>
--- tests/unit/stundenkontoService.spec.js
+++ tests/unit/stundenkontoService.spec.js
@@ -1,11 +1,12 @@
 
 import stundenkontoService from "@/services/stundenkontoService";
 import moment from "moment-timezone";
+import axios from "axios";
 
 
 
 
-
+jest.mock("axios")
 
 describe("Axios Requests",()=>{
 
@@ -19,7 +20,7 @@ describe("Axios Requests",()=>{
                 type: "sickness"
             }
         )
-
+        axios.get.mockResolvedValueOnce(proposal)
         const result = await stundenkontoService.getAbsence(userid)
         expect(result).toEqual(proposal)
 
@@ -28,20 +29,32 @@ describe("Axios Requests",()=>{
 })
 
 describe("Axios Requests",()=>{
+
     it ("get vacation and sickness",async ()=> {
         const userid = "123456789"
-        const proposal = JSON.stringify(
+        const mochresult = {"sickness": [{"endDate": "06-20-2022", "startDate": "06-12-2022", "type": "sickness", "userId": "1234"}], "vacation": [{"endDate": "06-20-2022", "startDate": "06-12-2022", "status": "approved", "type": "vacation", "userId": "1234"}]}
+        const proposal = ({data:[
+
             {
                 userId: "1234",
                 startDate: "06-12-2022",
                 endDate: "06-20-2022",
                 type: "sickness"
 
-            }
-        )
+            },
+            {
+                userId: "1234",
+                startDate: "06-12-2022",
+                endDate: "06-20-2022",
+                type: "vacation",
+                status:"approved"
 
+            }
+            ]}
+        )
+        axios.get.mockResolvedValueOnce(proposal)
         const result = await stundenkontoService.getvacationandsickness(JSON.stringify(userid))
-        expect(result).toEqual(JSON.stringify(proposal))
+        expect(result).toEqual(mochresult)
     })
 })
 describe("Axios Requests",()=>{
